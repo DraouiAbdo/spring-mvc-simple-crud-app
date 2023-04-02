0a8007f097745d3c851cc47239d6c2ddc775d054
abdelghani draoui <draoui.abdelghani@gmail.com>
2023-04-02 11:03:40 +0200

    Initial Commit

---
+++ src/main/java/dz/cut/springmvcsimpleapp/SpringMvcSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package dz.cut.springmvcsimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSimpleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSimpleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dz/cut/springmvcsimpleapp/SpringMvcSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.cut.springmvcsimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSimpleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
